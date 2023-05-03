3aa89735fb6fca084b1ba9a56467a9704657a681
Lucas Nobre <lucasnobre37@outlook.com>
2023-05-03 03:47:51 -0300

    improve input validation logic

--- Helpers/InputsValidations.php
+++ Helpers/InputsValidations.php
@@ -4,19 +4,19 @@ class InputsValidations
 {
     public static function validFloatInput(float $number): bool
     {
-        if ((!is_numeric($number)) || ($number > 3.402823E+38)) {
-            return false;
-        } else {
+        if ((is_numeric($number)) && ($number <= 3.402823E+38)) {
             return true;
+        } else {
+            return false;
         }
     }
 
     public static function validStringInput(string $text)
     {
-        if ((!strlen($text) < 0) || (!strlen($text) > 535)) {
-            return false;
-        } else {
+        if ((strlen($text) >= 0) && (!strlen($text) <= 535)) {
             return true;
+        } else {
+            return false;
         }
     }
 }
