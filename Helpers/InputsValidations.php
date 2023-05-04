aa72d23216f2fbdb2781dbf8e15ddb6dc3aa410b
Lucas Nobre <lucasnobre37@outlook.com>
2023-05-04 04:31:55 -0300

    update input validations helper functions

--- Helpers/InputsValidations.php
+++ Helpers/InputsValidations.php
@@ -6,17 +6,34 @@ class InputsValidations
     {
         if ((is_numeric($number)) && ($number <= 3.402823E+38)) {
             return true;
-        } else {
-            return false;
         }
+        return false;
+    }
+
+    public static function validIntInput(int $number): bool
+    {
+        if ((is_numeric($number)) && ($number <= 2147483647)) {
+            return true;
+        }
+
+        return false;
     }
 
     public static function validStringInput(string $text)
     {
-        if ((strlen($text) >= 0) && (!strlen($text) <= 535)) {
+        if ((strlen($text) >= 0) && (strlen($text) <= 535)) {
             return true;
-        } else {
-            return false;
         }
+
+        return false;
+    }
+
+    public static function validCategoryInput(string $category)
+    {
+        if (($category === "books") || ($category === "dvds") || ($category === "furnitures")) {
+            return true;
+        }
+
+        return false;
     }
 }
